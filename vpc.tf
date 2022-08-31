5313df144977287a489ebaba453cf20bce0464cc
chris tiesling <chris.tiesling.1@amfam.com>
2022-08-31 19:49:40 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,14 @@
-network_name = "gaurav-network"
-project_id = var.project
-subnets = [
-  {
-    subnet_name = "gaurav-subnet"
-    subnet_ip   = "10.100.10.0/24"
-    subnet_region = var.region
-  }
-] 
+module "network" {
+  source  = "app.terraform.io/amfam-cst/network/google"
+  version = "1.0.0"
+  # insert required variables here
+  network_name = "gaurav-network"
+  project_id = var.project
+  subnets = [
+    {
+      subnet_name = "gaurav-subnet"
+      subnet_ip   = "10.100.10.0/24"
+      subnet_region = var.region
+    }
+  ] 
+}
